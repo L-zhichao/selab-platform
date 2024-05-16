759c24d74fa2aebe8e4404f7fd82676d0a63dd55
lggpcmj <2101701559@qq.com>
2024-05-16 20:41:22 +0800

    <refactor>(recuritservice/dao/impl/RegistratinDaoImpl):实现了方法

--- src/main/java/tyut/selab/recruitservice/dao/impl/RegistrationDaoImpl.java
+++ src/main/java/tyut/selab/recruitservice/dao/impl/RegistrationDaoImpl.java
@@ -1,5 +1,6 @@
 package tyut.selab.recruitservice.dao.impl;
 
+import tyut.selab.recruitservice.dao.BaseDao;
 import tyut.selab.recruitservice.dao.RegistrationDao;
 import tyut.selab.recruitservice.domain.RegistrationForm;
 import tyut.selab.recruitservice.dto.RegistrationDto;
@@ -8,6 +9,7 @@ import tyut.selab.recruitservice.view.RegistrationVo;
 import java.util.List;
 
 public class RegistrationDaoImpl implements RegistrationDao {
+    BaseDao baseDao = new BaseDao();
     /**
      *  增加一个报名表
      * @param registrationForm
@@ -32,7 +34,26 @@ public class RegistrationDaoImpl implements RegistrationDao {
      * @return
      */
     public RegistrationForm selectByRegistrationId(Integer registrationId){
-        return null;
+        String sql = """
+                select id,
+                interview_id userId,
+                email,
+                phone,
+                intent_department intentDepartment,
+                grade,
+                classroom,
+                interview_time interviewTime,
+                introduce,
+                purpose,
+                remark,
+                init_time createTime,
+                update_time updateTime,
+                form registration_form
+                where userId = ?
+                """;
+        List<RegistrationForm> revList = baseDao.baseQuery(RegistrationForm.class, sql, registrationId);
+
+        return revList != null && revList.size() > 0 ? revList.get(0):null;
     }
 
     /**
@@ -41,7 +62,53 @@ public class RegistrationDaoImpl implements RegistrationDao {
      * @return
      */
     public RegistrationForm selectByIntervieweesName(String intervieweesName){
-        return null;
+        String sql = """
+                select id,
+                interview_id userId,
+                email,
+                phone,
+                intent_department intentDepartment,
+                grade,
+                classroom,
+                interview_time interviewTime,
+                introduce,
+                purpose,
+                remark,
+                init_time createTime,
+                update_time updateTime,
+                form registration_form
+                where userId = select user_id form sys_user where user_name = ?
+                """;
+        List<RegistrationForm> registrationForm = baseDao.baseQuery(RegistrationForm.class, sql, intervieweesName);
+        return registrationForm != null && registrationForm.size() > 0 ? registrationForm.get(0):null;
+    }
+
+    /**
+     *  查询面试者的报名表
+     * @param intervieweesName
+     * @return
+     */
+    @Override
+    public List<RegistrationForm> selectByIntervieweesNameList(String intervieweesName) {
+        String sql = """
+                select id,
+                interview_id userId,
+                email,
+                phone,
+                intent_department intentDepartment,
+                grade,
+                classroom,
+                interview_time interviewTime,
+                introduce,
+                purpose,
+                remark,
+                init_time createTime,
+                update_time updateTime,
+                form registration_form
+                where userId = select user_id form sys_user where user_name = ?
+                """;
+        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql, "%" + intervieweesName + "%");
+        return registrationForms != null && registrationForms.size() > 0 ? registrationForms:null;
     }
 
     /**
@@ -57,7 +124,25 @@ public class RegistrationDaoImpl implements RegistrationDao {
      * @return
      */
     public List<RegistrationForm> selectByIntentDepartment(Integer intentDepartment, Integer cur, Integer size){
-        return null;
+        String sql = """
+                select id,
+                interview_id userId,
+                email,
+                phone,
+                intent_department intentDepartment,
+                grade,
+                classroom,
+                interview_time interviewTime,
+                introduce,
+                purpose,
+                remark,
+                init_time createTime,
+                update_time updateTime,
+                form registration_form
+                where intentDepartment= ?
+                """;
+        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql,intentDepartment);
+        return registrationForms != null && registrationForms.size() > 0 ? registrationForms:null;
     }
 
     /**
