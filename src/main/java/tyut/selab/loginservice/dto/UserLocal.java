9148d931e232d034dbba3cc6e9d6bd2393fbc2c3
triumph77 <3363159288@qq.com>
2024-05-19 20:15:12 +0800

    <refactor> <新增按照请求用户权限来控制不同的功能，其他组接口还没写，暂时注释>

--- src/main/java/tyut/selab/loginservice/dto/UserLocal.java
+++ src/main/java/tyut/selab/loginservice/dto/UserLocal.java
@@ -14,4 +14,44 @@ public class UserLocal {
     private Integer groupId;
     private Integer roleId;
     private String token;
+
+    public Integer getRoleId(){
+        return roleId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
--- src/main/java/tyut/selab/taskservice/controller/TaskController.java
+++ src/main/java/tyut/selab/taskservice/controller/TaskController.java
@@ -1,13 +1,13 @@
 package tyut.selab.taskservice.controller;
 
 
-import com.alibaba.fastjson.JSON;
-import com.mysql.cj.x.protobuf.MysqlxCrud;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tyut.selab.loginservice.dto.UserLocal;
+import tyut.selab.loginservice.utils.SecurityUtil;
 import tyut.selab.taskservice.common.HttpStatus;
 import tyut.selab.taskservice.dto.TaskInfoDto;
 import tyut.selab.taskservice.myutils.WebUtil;
@@ -19,6 +19,7 @@ import tyut.selab.utils.Result;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "TaskController",urlPatterns = {"/task/*"})
@@ -115,11 +116,16 @@ public class TaskController extends HttpServlet {
     private Result<Void> save(HttpServletRequest request, HttpServletResponse response){
         TaskInfoDto taskInfoDto = WebUtil.readJson(request, TaskInfoDto.class);
 
-        //判断用户身份，不同身份发布任务的范围不一样，权限不够就返回error
+        //判断用户身份，不同身份发布任务的范围不一样，权限不够就返回error(其他组接口没有实现，先注释）
+//        int roleId = getUserMessage(request, response).getRoleId();
+//        if (roleId == 3 ){
+//            return Result.error(HttpStatus.UNAUTHORIZED,"普通用户不能发布任务");
+//        }
         //
         //
         //
         //
+
         //相同的用户发布的任务最大个数应该有限制
         //
         //
@@ -164,14 +170,48 @@ public class TaskController extends HttpServlet {
      */
    private Result queryMyTask(HttpServletRequest request,HttpServletResponse response){
 
+       //权限问题，超级管理员能查询所有的任务，管理员只能查看自己发布的任务
+       //没有传参数的话，超级管理员看到的是所有的任务，管理员看到的是自己发布的任务。
+       //传参数的话，超级管理员看到的是某个人的任务，管理员看到的只是自己的任务
+
+       //获取请求用户的相关信息
+//       UserLocal userMessage = getUserMessage(request, response);
+//       Integer roleId = userMessage.getRoleId();
+//       String userName = userMessage.getUserName();
+//
+//       if (roleId == 3 ){
+//           return Result.error(HttpStatus.UNAUTHORIZED,"普通用户没有发布的任务");
+//       }
+       //
+       //
+       //
+       //
 
        String publisherName = request.getParameter("publisherName");
        int cur = Integer.parseInt(request.getParameter("cur"));
        int size = Integer.parseInt(request.getParameter("size"));
        List<TaskInfoVo> taskInfoVoPage = null;
+
        if (publisherName == null){
            //查询所有
-           List<TaskInfoVo> taskInfoVos = taskInfoService.queryAllTask();
+           List<TaskInfoVo> taskInfoVosOrigin = taskInfoService.queryAllTask();
+
+           //判断管理员的权限。超级管理员就查看所有，普通管理员就查看自己的
+//           List<TaskInfoVo> taskInfoVos = new ArrayList<>();
+//           if (roleId == 1){
+//               taskInfoVos = taskInfoVosOrigin;
+//           }else {
+//               for (TaskInfoVo taskInfoVo : taskInfoVosOrigin) {
+//                   if (taskInfoVo.getPublisherName().equals(userName)){
+//                       taskInfoVos.add(taskInfoVo);
+//                   }
+//               }
+//           }
+           //
+           //
+           //
+           //
+           List<TaskInfoVo> taskInfoVos = taskInfoVosOrigin;
 
            //判断前端查询的页面是否存在，如果不存在，返回空集合
            int beginIndex = (cur - 1) * size;
@@ -185,9 +225,29 @@ public class TaskController extends HttpServlet {
                //该页面的数据满足size个
                taskInfoVoPage = taskInfoVos.subList(beginIndex,endIndex);
            }
+
        }else{
-           //查询本人发布的任务信息
-           List<TaskInfoVo> taskInfoVos = taskInfoService.queryTaskInfoBypublish(publisherName);
+           //所查询的管理员发布的任务信息
+           List<TaskInfoVo> taskInfoVosOrigin = taskInfoService.queryTaskInfoBypublish(publisherName);
+
+           //判断管理员的权限。如果是超级管理员就能查别人发布的任务，如果是普通管理员就只能查看自己发布的任务
+//           List<TaskInfoVo> taskInfoVos = new ArrayList<>();
+//           if (roleId == 1 ){
+//               taskInfoVos = taskInfoVosOrigin;
+//           }else{
+//               //判断所差的管理员名称和请求管理员是否相同
+//               if (userName.equals(publisherName)){
+//                   taskInfoVos = taskInfoVosOrigin;
+//               }else{
+//                   return Result.error(HttpStatus.UNAUTHORIZED,"普通管理员不能查看其他管理员发布的任务");
+//               }
+//           }
+           //
+           //
+           //
+           //
+           List<TaskInfoVo> taskInfoVos = taskInfoVosOrigin;
+
            //判断前端查询的页面是否存在，如果不存在，返回空集合
            int beginIndex = (cur - 1) * size;
            int endIndex = cur * size;
@@ -212,17 +272,27 @@ public class TaskController extends HttpServlet {
      * @return
      */
   private Result update(HttpServletRequest request,HttpServletResponse response){
-      //如果不是任务发布者或者是超级管理员的话，权限不够禁止修改
-      //
-      //
-      //
-      //
 
 
       //获取前端传入的taskid
       String requestURI = request.getRequestURI();
       String[] split = requestURI.split("/");
       int taskId = Integer.parseInt(split[split.length - 1]);
+
+
+
+      //如果不是任务发布者或者是超级管理员的话，权限不够禁止修改
+//      UserLocal loginUser = getUserMessage(request, response);
+//      Integer roleId = loginUser.getRoleId();
+//      String userName = loginUser.getUserName();
+//      TaskInfoVo taskInfoVo = taskInfoService.queryById(taskId);
+//      if (!(taskInfoVo.getPublisherName().equals(userName)) && roleId != 1){
+//          return Result.error(HttpStatus.UNAUTHORIZED,"权限不够,禁止修改");
+//      }
+      //
+      //
+      //
+      //
 
 
       TaskInfoDto taskInfoDto = WebUtil.readJson(request, TaskInfoDto.class);
@@ -247,18 +317,63 @@ public class TaskController extends HttpServlet {
      * @return
      */
     private Result<TaskInfoVo> queryById(HttpServletRequest request, HttpServletResponse response) {
-        //权限问题，超级管理员能查询所有的任务id，管理员只能查看自己发布的任务，用户只能查看自己接收的任务
-        //
-        //
 
 
         int taskId = Integer.parseInt(request.getParameter("taskId"));
         TaskInfoVo taskInfoVo = taskInfoService.queryById(taskId);
-        if (taskInfoVo != null) {
-            return Result.success(taskInfoVo);
+        if (taskInfoVo == null){
+            return Result.error(HttpStatus.NOT_FOUND, "该任务不存在");
         }
 
-        return Result.error(HttpStatus.NOT_FOUND, "该任务不存在");
+//        //权限问题，超级管理员能查询所有的任务id，管理员只能查看自己发布的任务，用户只能查看自己接收的任务
+//        //
+//        boolean flag = false;
+//        //获取请求用户的相关信息
+//        UserLocal userMessage = getUserMessage(request, response);
+//        Integer roleId = userMessage.getRoleId();
+//        String userName = userMessage.getUserName();
+//        //判断请求用户是否为超级管理员
+//        if (roleId == 1 ){
+//            flag = true;
+//        }
+//        if(!flag) {
+//            //判断请求用户的查询任务是否是该用户所接取的任务之一
+//            Integer userId = userMessage.getUserId();
+//            //未实现，等别人写完queryTaskInfoByUserId接口后再写
+//            //查询用户收到的信息
+//                //查询用户所在的小组
+//                //查小组对应的任务id
+//                //查询task_info表格返回taskinfo对象
+//                //将查询到的taskinfo对象封装成一个taskinfovo对象，放在list集合中返回
+//            List<TaskInfoVo> taskInfoVos1 = taskInfoService.queryTaskInfoByUserId(userId);
+//            for (TaskInfoVo taskInfoVo1 : taskInfoVos1) {
+//                if (taskInfoVo1.getId() == taskId) {
+//                    flag = true;
+//                }
+//            }
+//        }
+//        if (!flag){
+//            //判断请求用户是否为任务的发布者
+//            List<TaskInfoVo> taskInfoVos2 = taskInfoService.queryTaskInfoBypublish(userName);
+//            for (TaskInfoVo taskInfoVo2 : taskInfoVos2) {
+//                if (taskInfoVo2.getId() == taskId) {
+//                    flag = true;
+//                }
+//            }
+//        }
+//
+//
+//        if (flag){
+//            return Result.success(taskInfoVo);
+//        }else{
+//            return Result.error(HttpStatus.UNAUTHORIZED,"权限不允许查询");
+//        }
+        //
+        //
+        //
+        //
+        return Result.success(taskInfoVo);
+
     }
 
 
@@ -269,24 +384,28 @@ public class TaskController extends HttpServlet {
      * @return
      */
     private Result delete(HttpServletRequest request,HttpServletResponse response){
-        //从请求中获取taskId
-       String taskId= request.getParameter("taskId");
-        if (taskId == null || taskId.isEmpty()) {
-            // 如果 taskId 为空或不存在，返回错误结果
-            return resultMaker.error(resultMaker.getCode(), resultMaker.getMsg());
-        }
-        else return resultMaker;
+//        //从请求中获取taskId
+//       String taskId= request.getParameter("taskId");
+//        if (taskId == null || taskId.isEmpty()) {
+//            // 如果 taskId 为空或不存在，返回错误结果
+//            return resultMaker.error(resultMaker.getCode(), resultMaker.getMsg());
+//        }
+//        else return resultMaker;
+        return null;
     }
 
 
 
     /**
-     * 进行身份认定，返回用户的身份角色id
+     * 进行身份认定，返回用户的身份信息
      * @param request
      * @param response
-     * @return
+     * @return 返回一个UserLocal对象
+     *  user 中的 roleId   1 标识超级管理员，返回 2 标识管理员，返回 3 表示普通用户
      */
-    private int roleId(HttpServletRequest request,HttpServletResponse response){
-        return 0;
+    private UserLocal getUserMessage(HttpServletRequest request,HttpServletResponse response){
+        String authorization = request.getHeader("Authorization");
+        UserLocal user = SecurityUtil.getUser(authorization);
+        return user;
     }
 }
