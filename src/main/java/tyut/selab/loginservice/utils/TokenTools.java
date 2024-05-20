2a0d4b5ac1c04df6f83d3d87df5cb474eb591c40
junyue <982293013@qq.com>
2024-05-20 21:48:23 +0800

    加了token操作工具

---
+++ src/main/java/tyut/selab/loginservice/utils/TokenTools.java
@@ -0,0 +1,47 @@
+package tyut.selab.loginservice.utils;
+
+import com.alibaba.druid.util.StringUtils;
+import tyut.selab.loginservice.utils.TokenProcessor;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenTools {
+    /**
+     * 生成token放入session
+     * @param request
+     * @param tokenServerkey
+     */
+    public static void createToken(HttpServletRequest request,String tokenServerkey){
+        String token = TokenProcessor.getInstance().makeToken();
+        request.getSession().setAttribute(tokenServerkey, token);
+    }
+    /**
+     * 移除token
+     * @param request
+     * @param tokenServerkey
+     */
+    public static void removeToken(HttpServletRequest request,String tokenServerkey){
+        request.getSession().removeAttribute(tokenServerkey);
+    }
+    /**
+     * 判断请求参数中的token是否和session中一致
+     * @param request
+     * @param tokenClientkey
+     * @param tokenServerkey
+     * @return
+     */
+    public static boolean judgeTokenIsEqual(HttpServletRequest request, String tokenClientkey, String tokenServerkey){
+        String token_client = request.getParameter(tokenClientkey);
+        if(StringUtils.isEmpty(token_client)){
+            return false;
+        }
+        String token_server = (String) request.getSession().getAttribute(tokenServerkey);
+        if(StringUtils.isEmpty(token_server)){
+            return false;
+        }
+        if(!token_server.equals(token_client)){
+            return false;
+        }
+        return true;
+    }
+}
