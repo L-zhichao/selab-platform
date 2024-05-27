f1b3c888bddb32da42c8fc883083913235733fbd
1335625798 <1335625798@qq.com>
2024-05-27 19:43:53 +0800

    modify(daoTest.java): <others>

--- src/main/java/tyut/selab/loginservice/controller/LoginController.java
+++ src/main/java/tyut/selab/loginservice/controller/LoginController.java
@@ -1,12 +1,12 @@
 package tyut.selab.loginservice.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import tyut.selab.utils.Result;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
