9e4736e996eb027f5da8aeabe1904703b3a38547
1335625798 <1335625798@qq.com>
2024-05-27 19:44:03 +0800

    modify(daoTest.java): <others>

--- src/main/java/tyut/selab/recruitservice/controller/RegistrationController.java
+++ src/main/java/tyut/selab/recruitservice/controller/RegistrationController.java
@@ -1,27 +1,38 @@
 package tyut.selab.recruitservice.controller;
-
+import com.alibaba.druid.support.json.JSONParser;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import tyut.selab.recruitservice.dto.RegistrationDto;
+import tyut.selab.recruitservice.service.InsertException;
+import tyut.selab.recruitservice.service.UpdateException;
 import tyut.selab.recruitservice.service.impl.RegistrationServiceImpl;
 import tyut.selab.recruitservice.service.RegistrationService;
 import tyut.selab.recruitservice.view.RegistrationVo;
 import tyut.selab.utils.Result;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@WebServlet(name = "RegistrationController",urlPatterns = {})
+@WebServlet (name = "RegistrationController",urlPatterns = {"/registration/insertRegistration","/registration/updateRegistration","/registration/queryMyRecruit"})
 public class RegistrationController extends HttpServlet {
-    private RegistrationService RegistrationService = new RegistrationServiceImpl();
+    private RegistrationService registrationService = new RegistrationServiceImpl();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        Integer userId = Integer.valueOf(req.getParameter("userId"));
+        String requestURI = req.getRequestURI();
+        String []split =requestURI.split("/");
+        String methodName =split[split.length-1];
+        if(methodName.equals("queryMyRecruit")){
+
+        }
     }
 
     @Override
@@ -32,14 +43,27 @@ public class RegistrationController extends HttpServlet {
         while((line=reader.readLine())!=null){
             jsonRequest.append(line);
         }
-        ObjectMapper objectMapper =new ObjectMapper();
+        String requestBody = jsonRequest.toString();
         String requestURI = req.getRequestURI();
         String []split =requestURI.split("/");
         String methodName =split[split.length-1];
+        System.out.println(requestBody);
         if(methodName.equals("insertRegistration")){
-            RegistrationDto registrationDto
+            RegistrationDto registrationDto = JSONObject.parseObject(requestBody,RegistrationDto.class);
+            System.out.println(registrationDto);
+            Result<RegistrationDto> result = save(req,resp,registrationDto);
+            Map res = new HashMap<>();
+            res.put("code",result.getCode().toString());
+            result.setMsg("6666666");
+            res.put("msg",result.getMsg().toString());
+            resp.getWriter().write(JSON.toJSONString(res));
         } else if (methodName.equals("updateRegistration")) {
-
+            RegistrationVo registrationVo = JSON.parseObject(requestBody,RegistrationVo.class);
+            Result<RegistrationVo> result = update(req,resp,registrationVo);
+            Map res = new HashMap<>();
+            res.put("code",result.getCode().toString());
+            res.put("msg",result.getMsg().toString());
+            resp.getWriter().write(JSON.toJSONString(res));
         }
     }
 
@@ -49,8 +73,15 @@ public class RegistrationController extends HttpServlet {
      * @param response
      * @return
      */
-    private Result save(HttpServletRequest request,HttpServletResponse response){
-        RegistrationDto registrationDto;
+    private Result save(HttpServletRequest request,HttpServletResponse response,RegistrationDto registrationDto){
+       try{registrationService.insertRegistration(registrationDto);
+           Result<RegistrationDto> result = new Result<>(200,registrationDto);
+           return result;
+       }catch(InsertException e){
+           System.out.println("插入失败");
+           Result<RegistrationDto> result = new Result<>(303,registrationDto);
+           return result;
+       }
     }
 
     /**
@@ -58,8 +89,15 @@ public class RegistrationController extends HttpServlet {
      * param: registrationVo
      * @return
      */
-    private Result update(HttpServletRequest request,HttpServletResponse response){
-        RegistrationVo registrationVo;
+    private Result update(HttpServletRequest request,HttpServletResponse response,RegistrationVo registrationVo){
+        try{registrationService.updateRegistration(registrationVo);
+            Result<RegistrationVo> result = new Result<>(200,registrationVo);
+            return result;
+        }catch(UpdateException e){
+            System.out.println("修改失败");
+            Result<RegistrationVo> result = new Result<>(303,registrationVo);
+            return result;
+        }
     }
 
     /**
@@ -112,8 +150,10 @@ public class RegistrationController extends HttpServlet {
      *  查询本人提交报名表 【userId通过后端获取】
      * @return
      */
-    private Result queryMy(HttpServletRequest request,HttpServletResponse response){
+    private Result queryMy(HttpServletRequest request,HttpServletResponse response,Integer userId){
+        registrationService.queryMyRecruit(userId);
         return null;
+
     }
 
 
