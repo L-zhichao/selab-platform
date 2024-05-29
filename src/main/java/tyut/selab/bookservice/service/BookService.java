ea9748f44e795e79a8d3f597bfe98f4454d8c0d8
Thefish <fish159632874@proton.me>
2024-05-29 20:12:01 +0800

    refactor(BookService):新增了bookIofoToBookVo()方法

--- src/main/java/tyut/selab/bookservice/service/BookService.java
+++ src/main/java/tyut/selab/bookservice/service/BookService.java
@@ -3,9 +3,8 @@ package tyut.selab.bookservice.service;
 import tyut.selab.bookservice.domain.BookInfo;
 import tyut.selab.bookservice.dto.BookDto;
 import tyut.selab.bookservice.vo.BookVo;
+import tyut.selab.utils.PageUtil;
 
-import java.awt.print.Book;
-import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -24,42 +23,35 @@ public interface BookService {
      * @return
      */
 
-    public Integer insertBook(BookDto bookDto) throws SQLException;
+    public Integer insertBook(BookDto bookDto);
 
     /**
      *   修改书籍信息(管理员操作)
      * @param bookVo
      * @return
      */
-    public Integer updateBook(BookVo bookVo) throws SQLException;
-
-    /**
-     *  删除书籍
-     * @param bookId
-     * @return
-     */
-    public Integer deleteBook(Integer bookId) throws SQLException;
+    public Integer updateBook(BookVo bookVo);
 
     /**
      * 分页查询所有书籍
      *
      * @return
      */
-    public List<BookVo> selectList(Integer cur, Integer size);
+    public PageUtil<BookVo> selectList(Integer curx, Integer size);
 
     /**
      *   通过 bookId 查询书籍信息
      * @param bookId 书籍id
      * @return
      */
-    public BookVo selectBookById(Integer bookId) throws SQLException, NoSuchFieldException, InstantiationException, IllegalAccessException;
+    public BookVo selectBookById(Integer bookId);
 
     /**
      *   通过书籍名称查询书籍信息(支持模糊查询)
      * @param bookName
      * @return
      */
-    public List<BookVo> selectBookByBookName(String bookName);
+    public BookVo selectBookByBookName(String bookName);
 
     /**
      *  通过userid查询用户所拥有的所有书籍
