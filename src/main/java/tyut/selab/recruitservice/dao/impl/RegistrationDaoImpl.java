52e04c8489f06c0dc0d34b809f78806ecfe66156
lggpcmj <2101701559@qq.com>
2024-05-29 20:42:58 +0800

    <refactor>(recuritservice/dao/impl) 完善了dao层

--- src/main/java/tyut/selab/recruitservice/dao/impl/RegistrationDaoImpl.java
+++ src/main/java/tyut/selab/recruitservice/dao/impl/RegistrationDaoImpl.java
@@ -3,9 +3,7 @@ package tyut.selab.recruitservice.dao.impl;
 import tyut.selab.recruitservice.dao.BaseDao;
 import tyut.selab.recruitservice.dao.RegistrationDao;
 import tyut.selab.recruitservice.domain.RegistrationForm;
-import tyut.selab.recruitservice.dto.RegistrationDto;
-import tyut.selab.recruitservice.view.RegistrationVo;
-import tyut.selab.userservice.vo.UserVo;
+import tyut.selab.utils.PageUtil;
 
 import java.util.List;
 
@@ -40,7 +38,7 @@ public class RegistrationDaoImpl implements RegistrationDao {
     public RegistrationForm selectByRegistrationId(Integer registrationId){
         String sql = """
                 select id,
-                interview_id userId,
+                interview_id as IntervieweesId,
                 email,
                 phone,
                 intent_department intentDepartment,
@@ -50,70 +48,73 @@ public class RegistrationDaoImpl implements RegistrationDao {
                 introduce,
                 purpose,
                 remark,
-                init_time createTime,
+                init_time initTime,
                 update_time updateTime
                 from selab_platform.registration_form
-                where userId = ?
+                where interview_id = ? and
+                selab_platform.registration_form.del_flag <> 1
                 """;
         List<RegistrationForm> revList = baseDao.baseQuery(RegistrationForm.class, sql, registrationId);
-
         return revList != null && revList.size() > 0 ? revList.get(0):null;
     }
 
     /**
-     *  查询面试者的报名表
+     *  通过面试者姓名查询报名表
      * @param intervieweesName
      * @return
      */
     @Override
-    public List<RegistrationForm> selectByIntervieweesName(String intervieweesName){
+    public PageUtil<RegistrationForm> selectByIntervieweesName(String intervieweesName,Integer cur,Integer size){
         String sql = """
-                select id,
-                interview_id userId,
-                email,
-                phone,
-                intent_department intentDepartment,
-                grade,
-                classroom,
-                interview_time interviewTime,
-                introduce,
-                purpose,
-                remark,
-                init_time createTime,
-                update_time updateTime
-                from selab_platform.registration_form
-                where userId = select user_id form sys_user where user_name = ?
+              select selab_platform.registration_form.id,
+                     selab_platform.registration_form.interview_id IntervieweesId,
+                     selab_platform.registration_form.email,
+                     selab_platform.registration_form.phone,
+                     selab_platform.registration_form.intent_department intentDepartment,
+                     selab_platform.registration_form.grade,
+                     selab_platform.registration_form.classroom,
+                     selab_platform.registration_form.interview_time interviewTime,
+                     selab_platform.registration_form.introduce,
+                     selab_platform.registration_form.purpose,
+                     selab_platform.registration_form.remark,
+                     selab_platform.registration_form.init_time initTime,
+                     selab_platform.registration_form.update_time updateTime
+              from   selab_platform.registration_form,
+                     selab_platform.sys_user
+              where  selab_platform.registration_form.interview_id = selab_platform.sys_user.user_id and
+                     selab_platform.sys_user.user_name like '%s' and
+                     selab_platform.registration_form.del_flag <> 1
+              limit ?,?
                 """;
-        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql, intervieweesName);
-        return registrationForms != null && registrationForms.size() > 0 ? registrationForms : null;
+        sql = String.format(sql, "%"+intervieweesName+"%");
+        Integer index = (cur - 1) * size;
+        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class,sql,index,size);;
+        String sql2 = """
+                select count(*)
+                from selab_platform.registration_form,
+                     selab_platform.sys_user
+                where selab_platform.registration_form.interview_id = selab_platform.sys_user.user_id and
+                     selab_platform.sys_user.user_name like '%s' and
+                     selab_platform.registration_form.del_flag <> 1
+                """;
+        sql2 = String.format(sql2, "%"+intervieweesName+"%");
+        Integer total = Integer.parseInt(String.valueOf(baseDao.baseQueryObject(Long.class, sql2)));
+        PageUtil<RegistrationForm> pageUtil = new PageUtil<>();
+        pageUtil.setCur(cur);
+        pageUtil.setSize(size);
+        pageUtil.setData(registrationForms);
+        pageUtil.setTotal(total);
+        return registrationForms != null && !registrationForms.isEmpty() ? pageUtil:null;
     }
 
     /**
-     *  查询面试者的报名表
+     * 查询面试者的报名表
+     *
      * @param intervieweesName
      * @return
      */
     @Override
     public List<RegistrationForm> selectList(String intervieweesName) {
-//        String sql = """
-//                select id,
-//                interview_id userId,
-//                email,
-//                phone,
-//                intent_department intentDepartment,
-//                grade,
-//                classroom,
-//                interview_time interviewTime,
-//                introduce,
-//                purpose,
-//                remark,
-//                init_time createTime,
-//                update_time updateTime,
-//                from registration_form
-//                where userId = select user_id form sys_user where user_name = ?
-//                """;
-//        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql, "%" + intervieweesName + "%");
-//        return registrationForms != null && registrationForms.size() > 0 ? registrationForms:null;
         return null;
     }
 
@@ -122,10 +123,10 @@ public class RegistrationDaoImpl implements RegistrationDao {
      * @return
      */
     @Override
-    public List<RegistrationForm> selectAll(Integer cur, Integer size){
+    public PageUtil<RegistrationForm> selectAll(Integer cur, Integer size){
         String sql = """
                 select id,
-                interview_id userId,
+                interview_id IntervieweesId,
                 email,
                 phone,
                 intent_department intentDepartment,
@@ -135,12 +136,26 @@ public class RegistrationDaoImpl implements RegistrationDao {
                 introduce,
                 purpose,
                 remark,
-                init_time createTime,
+                init_time initTime,
                 update_time updateTime
                 from selab_platform.registration_form
+                where selab_platform.registration_form.del_flag <> 1
+                limit ?,?
                 """;
-        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql);
-        return registrationForms != null && registrationForms.size() > 0 ? registrationForms:null;
+        Integer index = (cur - 1) * size;
+        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql,index,size);
+        String sql2 = """
+                select count(*)
+                from selab_platform.registration_form
+                where selab_platform.registration_form.del_flag <> 1
+                """;
+        Integer total = Integer.parseInt(String.valueOf(baseDao.baseQueryObject(Long.class, sql2)));
+        PageUtil<RegistrationForm> pageUtil = new PageUtil<>();
+        pageUtil.setCur(cur);
+        pageUtil.setSize(size);
+        pageUtil.setData(registrationForms);
+        pageUtil.setTotal(total);
+        return registrationForms != null && !registrationForms.isEmpty() ? pageUtil:null;
     }
 
     /**
@@ -148,10 +163,10 @@ public class RegistrationDaoImpl implements RegistrationDao {
      * @return
      */
     @Override
-    public List<RegistrationForm> selectByIntentDepartment(Integer intentDepartment, Integer cur, Integer size){
+    public PageUtil<RegistrationForm> selectByIntentDepartment(Integer intentDepartment, Integer cur, Integer size){
         String sql = """
                 select id,
-                interview_id userId,
+                interview_id IntervieweesId,
                 email,
                 phone,
                 intent_department intentDepartment,
@@ -161,13 +176,28 @@ public class RegistrationDaoImpl implements RegistrationDao {
                 introduce,
                 purpose,
                 remark,
-                init_time createTime,
+                init_time initTime,
                 update_time updateTime
                 from selab_platform.registration_form
-                where intentDepartment = ?
+                where intent_department = ? and
+                selab_platform.registration_form.del_flag <> 1
+                limit ?,?
                 """;
-        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql,intentDepartment);
-        return registrationForms != null && registrationForms.size() > 0 ? registrationForms:null;
+        Integer index = (cur - 1) * size;
+        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class,sql,intentDepartment,index,size);
+        String sql2 = """
+                select count(*)
+                from selab_platform.registration_form
+                where intent_department = ? and
+                selab_platform.registration_form.del_flag <> 1
+                """;
+        Integer total = Integer.parseInt(String.valueOf(baseDao.baseQueryObject(Long.class, sql2,intentDepartment)));
+        PageUtil<RegistrationForm> pageUtil = new PageUtil<>();
+        pageUtil.setCur(cur);
+        pageUtil.setSize(size);
+        pageUtil.setData(registrationForms);
+        pageUtil.setTotal(total);
+        return registrationForms != null && !registrationForms.isEmpty() ? pageUtil:null;
     }
 
     /**
@@ -176,10 +206,10 @@ public class RegistrationDaoImpl implements RegistrationDao {
      * @return
      */
     @Override
-    public List<RegistrationForm> selectByGradeId(Integer grade, Integer cur, Integer size){
+    public PageUtil<RegistrationForm> selectByGradeId(Integer grade, Integer cur, Integer size){
         String sql = """
                 select id,
-                interview_id userId,
+                interview_id IntervieweesId,
                 email,
                 phone,
                 intent_department intentDepartment,
@@ -189,13 +219,28 @@ public class RegistrationDaoImpl implements RegistrationDao {
                 introduce,
                 purpose,
                 remark,
-                init_time createTime,
+                init_time initTime,
                 update_time updateTime
                 from selab_platform.registration_form
-                where grade = ?
+                where grade = ? and
+                selab_platform.registration_form.del_flag <> 1
+                limit ?,?
                 """;
-        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql,grade);
-        return registrationForms != null && registrationForms.size() > 0 ? registrationForms:null;
+        Integer index = (cur - 1) * size;
+        List<RegistrationForm> registrationForms = baseDao.baseQuery(RegistrationForm.class, sql,grade,index,size);
+        String sql2 = """
+                select count(*)
+                from selab_platform.registration_form
+                where grade = ? and
+                selab_platform.registration_form.del_flag <> 1
+                """;
+        Integer total = Integer.parseInt(String.valueOf(baseDao.baseQueryObject(Long.class, sql2,grade)));
+        PageUtil<RegistrationForm> pageUtil = new PageUtil<>();
+        pageUtil.setCur(cur);
+        pageUtil.setSize(size);
+        pageUtil.setData(registrationForms);
+        pageUtil.setTotal(total);
+        return registrationForms != null && !registrationForms.isEmpty() ? pageUtil:null;
     }
 
 }
