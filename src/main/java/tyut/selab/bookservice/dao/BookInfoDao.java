2d460f9b568cd526c9beb78f0ec51924bbae2e50
Thefish <fish159632874@proton.me>
2024-05-29 19:58:28 +0800

    feat(BookInfoDao):完成了selectAll()方法

--- src/main/java/tyut/selab/bookservice/dao/BookInfoDao.java
+++ src/main/java/tyut/selab/bookservice/dao/BookInfoDao.java
@@ -1,10 +1,7 @@
 package tyut.selab.bookservice.dao;
 
 import tyut.selab.bookservice.domain.BookInfo;
-import tyut.selab.bookservice.vo.BookVo;
 
-import java.awt.print.Book;
-import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -20,53 +17,56 @@ public interface BookInfoDao {
      * @param bookInfo
      * @return
      */
-    public Integer insert(BookInfo bookInfo) throws SQLException;
+    public Integer insert(BookInfo bookInfo);
 
     /**
      *  修改书籍信息
      * @param bookInfo
      * @return
      */
-    public Integer update(BookInfo bookInfo) throws SQLException;
+    public Integer update(BookInfo bookInfo);
 
     /**
      *  删除图书信息
      * @param bookId
      * @return
      */
-    public Integer delete(Integer bookId) throws SQLException;
+    public Integer delete(Integer bookId);
 
     /**
      * 通过id查询书籍信息
      * @param bookId
      * @return
      */
-    public BookInfo selectByBookIdBookInfo(Integer bookId) throws SQLException;
+    public BookInfo selectByBookIdBookInfo(Integer bookId);
 
     /**
      *  通过拥有者id查询本人所有书籍
      * @param userId
      * @return
      */
-    public List<BookInfo> selectByOwnerBookInfo(Integer userId) throws SQLException;
+    public List<BookInfo> selectByOwnerBookInfo(Integer userId);
 
     /**
      * 查询所有书籍
      * @return
      */
-    public List<BookInfo> selectAll(Integer cur, Integer size) throws SQLException;
+    public List<BookInfo> selectAll(Integer cur, Integer size);
 
     /**
      * 通过书籍名称<模糊查询>书籍信息
      * @return
      */
-    public List<BookInfo> selectAllByBookName(String bookName) throws SQLException;
+    public List<BookInfo> selectAllByBookName();
+
 
     /**
      *  通过书籍名称和拥有者id查询书籍信息
      * @return
      */
-    public List<BookInfo> selectByOwnerBookName(Integer userId, String bookName) throws SQLException;
+    public List<BookInfo> selectByOwnerBookName(Integer userId, String bookName);
+
+    public Integer selectAllCount();
 }
 
 
