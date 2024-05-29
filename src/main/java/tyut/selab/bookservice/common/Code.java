9f6ed57fefff5663afb86ba59128b02ac8cd2299
Thefish <fish159632874@proton.me>
2024-05-29 11:18:06 +0800

    feat(Code):新增了错误码

--- src/main/java/tyut/selab/bookservice/common/Code.java
+++ src/main/java/tyut/selab/bookservice/common/Code.java
@@ -10,4 +10,15 @@ package tyut.selab.bookservice.common;
 public interface Code {
     public static final Integer SUCCESS = 200;
     public static final Integer DATABASE_HAND_UNKNOWN_ERROR = 50001;
+    public static final Integer MESSAGE_ERROR = 500001;
+    public static final Integer PATH_ERROR = 500004;
+    public static final Integer OTHER_BORRORED = 500006;
+    public static final Integer CAN_NOT_BORROWED = 500007;
+    public static final Integer ALREADY_BORROWED = 500008;
+    public static final Integer NOT_FIND_BOOK = 500009;
+    public static final Integer NO_RECORDED = 500010;
+    public static final Integer HAVE_NOT_RECORDED = 500011;
+    public static final Integer ALREADY_RETURNED = 500012;
+
+
 }
