ca7b91ddf74deed70ee35a7126971944c92e2d58
MIKE <2335459579@qq.com>
2024-06-01 17:23:07 +0800

    添加了判断

--- src/main/java/tyut/selab/bookservice/dao/impl/BorrowBookDaoImpl.java
+++ src/main/java/tyut/selab/bookservice/dao/impl/BorrowBookDaoImpl.java
@@ -33,7 +33,11 @@ public class BorrowBookDaoImpl implements BorrowBookDao {
     public BorrowBook selectByBorrowIdBorrowBook(Integer borrowId) {
         String sql = "select borrow_Id borrowId,book_id bookId,borrow_user borrowUser,borrow_duration borrowDuration,status,borrow_time borrowTime,return_time returnTime from book_borrow where borrow_id = ?";
         List<BorrowBook> borrowBooks = baseDao.baseQuery(BorrowBook.class, sql, borrowId);
-        return borrowBooks.get(0);
+        if (!borrowBooks.isEmpty()){
+            return borrowBooks.get(0);
+        }else {
+            return null;
+        }
     }
 
     @Override
@@ -106,10 +110,4 @@ public class BorrowBookDaoImpl implements BorrowBookDao {
         return Integer.parseInt(String.valueOf((baseDao.baseQueryObject(Long.class,sql))));
     }
 
-    @Override
-    public List<BorrowBook> selectAllByBorrowId(Integer borrowId) {
-        String sql = "select borrow_id borrowId,book_id bookId,borrow_user borrowUser,borrow_duration borrowDuration,status,borrow_time borrowTime,return_time returnTime from book_borrow where borrow_id = ?;";
-        List<BorrowBook> borrowBooks = baseDao.baseQuery(BorrowBook.class, sql, borrowId);
-        return borrowBooks;
-    }
 }
