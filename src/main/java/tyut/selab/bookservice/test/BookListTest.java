beee0d368ca60433f4fbdd876a95a3a09e0578ef
Thefish <fish159632874@proton.me>
2024-06-06 16:55:23 +0800

    refactor(BookListTest):增加了一些test

--- src/main/java/tyut/selab/bookservice/test/BookListTest.java
+++ src/main/java/tyut/selab/bookservice/test/BookListTest.java
@@ -1,6 +1,7 @@
 package tyut.selab.bookservice.test;
 
 import org.junit.jupiter.api.Test;
+import tyut.selab.bookservice.controller.BookController;
 import tyut.selab.utils.Result;
 
 /**
@@ -18,5 +19,12 @@ public class BookListTest {
         Result<Object> objectResult = new Result<>(200,"{bookname = 《nihao》,Author = zhangsan}");
         objectResult.setMsg("success");
         System.out.println(objectResult);
+    }
+//    BookController bookController = new BookController();
+    @Test
+    public void testJson(){
+        System.out.println(BookController.isDouble("12"));
+        System.out.println(BookController.isInteger("12"));
+        System.out.println(BookController.isString("12121sdsad"));
     }
 }
