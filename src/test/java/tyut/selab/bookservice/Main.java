37d5063e60513474370862104e01bf30101cd93d
lizhichao <2658085067@qq.com>
2024-06-11 18:20:11 +0800

    init

---
+++ src/test/java/tyut/selab/bookservice/Main.java
@@ -0,0 +1,9 @@
+package tyut.selab.bookservice;/**
+ * @className:      Main
+ * @author:     lizhichao
+ * @description:  TODO  
+ * @date:    2024/5/22 19:15
+ * @version:    1.0
+ */ 
+public class Main {
+}
